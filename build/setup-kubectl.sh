ff5450d7909d94cb3c3d88ce22a4e8c16f77f344
Pete Koelbl <petekoelbl@gmail.com>
2022-08-27 03:54:27 -0500

    remove generated json files, removed weird characters from proj file, removed default version

--- build/setup-kubectl.sh
+++ build/setup-kubectl.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 set -e
 
-DEFAULT_KUBECTL_VERSION="v1.21.2"
-
-# Arguments: kubectl version
 function installKubectl {
-    local loc_kube_version=$1
-
-    if [ -z "$loc_kube_version" ]; then
-        loc_kube_version=$DEFAULT_KUBECTL_VERSION
-    fi
-
     apk update
     apk upgrade
 
-    apk add -u kubectl=${loc_kube_version}
+    apk add -u kubectl
 }
 
 if [ -z "$@" ]; then
