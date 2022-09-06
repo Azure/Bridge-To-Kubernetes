2f90e0db7f48bbef4b3822b0dd99434832424aa0
Hariharan Subramanian <105889062+hsubramanianaks@users.noreply.github.com>
2022-09-06 10:43:41 -0400

    Fix dependent bot (#11)
    fix for postcss bot alert

--- samples/todo-app/frontend/pages/index.js
+++ samples/todo-app/frontend/pages/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import TodoApp from '../components/TodoApp'
-import '../public/static/index.css'
 
 export default class Index extends Component {
     constructor(props) {
