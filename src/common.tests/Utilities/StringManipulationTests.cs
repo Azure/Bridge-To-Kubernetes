4333ac8ce37b4df16d7fc7da077194e565bea80f
elvilla <elvilla@microsoft.com>
2023-03-20 10:34:43 -0700

    Address PR comment

--- src/common.tests/Utilities/StringManipulationTests.cs
+++ src/common.tests/Utilities/StringManipulationTests.cs
@@ -13,9 +13,8 @@ namespace Microsoft.BridgeToKubernetes.Common.Tests.Utilities
         [Theory]
         [InlineData("BEGIN PRIVATE KEY\nuirethweuifhewiufhweiuofhweo\njweiorjweoifnewouhfousdgherufhsdiuhEND PRIVATE KEY\n", "KEY_WAS_REMOVED\n")]
         [InlineData("iurhuweifhoBEGIN PRIVATE KEY\nuirethweuifhewiufhweiuofhweo\njweiorjweoifnewouhfousdgherufhsdiuhEND PRIVATE KEY\n", "iurhuweifhoKEY_WAS_REMOVED\n")]
-
+        [InlineData("BEGIN PRIVATE KEYb2kkkkkkkkkkkkkkEND PRIVATE KEY", "KEY_WAS_REMOVED")]
         [InlineData("hello world", "hello world")]
-
         [InlineData("BEGIN PRIVATE KEY\nuirethweuifhewiufhweiuofhweo\njweiorjweoifnewouhfousdgherufhsdiuhEND PRIVATE KEY", "KEY_WAS_REMOVED")]
         public void RemovePrivateKeyIfNeededTest(string input, string expected)
         {
