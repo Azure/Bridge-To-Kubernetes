da3ea55121dec68ebdd231c6d2441357da487b0b
elenavillamil <mariaelena.villamil.rodriguez@gmail.com>
2023-03-21 09:16:18 -0700

    Updating logging property and update string manipulation (#208)
    * Updating logging property and update string manipulation
    
    * Address PR comment
    
    ---------
    
    Co-authored-by: elvilla <elvilla@microsoft.com>

---
+++ src/common.tests/Utilities/StringManipulationTests.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+// --------------------------------------------------------------------------------------------
+
+using Microsoft.BridgeToKubernetes.Common.Utilities;
+using Xunit;
+
+namespace Microsoft.BridgeToKubernetes.Common.Tests.Utilities
+{
+    public class StringManipulationTests
+    {
+        [Theory]
+        [InlineData("BEGIN PRIVATE KEY\nuirethweuifhewiufhweiuofhweo\njweiorjweoifnewouhfousdgherufhsdiuhEND PRIVATE KEY\n", "KEY_WAS_REMOVED\n")]
+        [InlineData("iurhuweifhoBEGIN PRIVATE KEY\nuirethweuifhewiufhweiuofhweo\njweiorjweoifnewouhfousdgherufhsdiuhEND PRIVATE KEY\n", "iurhuweifhoKEY_WAS_REMOVED\n")]
+        [InlineData("BEGIN PRIVATE KEYb2kkkkkkkkkkkkkkEND PRIVATE KEY", "KEY_WAS_REMOVED")]
+        [InlineData("hello world", "hello world")]
+        [InlineData("BEGIN PRIVATE KEY\nuirethweuifhewiufhweiuofhweo\njweiorjweoifnewouhfousdgherufhsdiuhEND PRIVATE KEY", "KEY_WAS_REMOVED")]
+        public void RemovePrivateKeyIfNeededTest(string input, string expected)
+        {
+            Assert.True(string.Equals(StringManipulation.RemovePrivateKeyIfNeeded(input), expected));
+        }
+    }
+}
