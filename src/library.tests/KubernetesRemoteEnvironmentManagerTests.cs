7e9f989698bbb89d2d0fd20533ee58f55ea53535
Hariharan Subramanian <105889062+hsubramanianaks@users.noreply.github.com>
2023-04-03 23:04:58 -0400

    update unit test cases.

--- src/library.tests/KubernetesRemoteEnvironmentManagerTests.cs
+++ src/library.tests/KubernetesRemoteEnvironmentManagerTests.cs
@@ -19,26 +19,28 @@ using Microsoft.BridgeToKubernetes.Common.Logging;
 using Microsoft.BridgeToKubernetes.Library.Connect.Environment;
 using Microsoft.BridgeToKubernetes.Common.IO;
 using System.Threading.Tasks;
+using FakeItEasy.Core;
+using System.Linq;
+using Microsoft.BridgeToKubernetes.Common.Models.Kubernetes;
 
 namespace Microsoft.BridgeToKubernetes.Library.Tests
 {
     public class KubernetesRemoteEnvironmentManagerTests : TestsBase
     {
-        private IRemoteEnvironmentManager _remoteEnvironmentManager;
+        private KubernetesRemoteEnvironmentManager _remoteEnvironmentManager;
+        private V1Pod testPod;
 
         public KubernetesRemoteEnvironmentManagerTests() 
         {
             var testEv = new EnvironmentVariables(new Platform());
 
-            var testRc = RemoteContainerConnectionDetails.CreatingNewPodWithContextFromExistingService("testNameSpace", "testService", "testRoutingHeader");
-            var testPod = new V1Pod();
-            testPod.Metadata = new V1ObjectMeta();
-            testPod.Metadata.Name = "testName";
+            var testRc = RemoteContainerConnectionDetails.CreatingNewPodWithContextFromExistingService("testNameSpace", "testService", "testRoutingHeader", "testContainer");
+            testPod = createFakePodObject();
             testRc.UpdatePodDetails(testPod);
             
             var remoteContainerConnectionDetails = new AsyncLazy<RemoteContainerConnectionDetails>(() => Task.FromResult(testRc));
             var environmentVariables = new AsyncLazy<IEnvironmentVariables>(() => testEv);
-            _remoteEnvironmentManager = _autoFake.Resolve<IRemoteEnvironmentManager>(TypedParameter.From(remoteContainerConnectionDetails), TypedParameter.From(environmentVariables));
+            _remoteEnvironmentManager = _autoFake.Resolve<KubernetesRemoteEnvironmentManager>(TypedParameter.From(remoteContainerConnectionDetails), TypedParameter.From(environmentVariables));
         }
 
         [Fact]
@@ -55,29 +57,47 @@ namespace Microsoft.BridgeToKubernetes.Library.Tests
         private void SetupTestMocks()
         {
             var testEv = new EnvironmentVariables(new Platform());
-            A.CallTo(() => _autoFake.Resolve<IKubernetesClient>().CreateV1PodAsync(A<string>._, A<V1Pod>._, A<CancellationToken>._)).Invokes((pod) => CheckPodEnvVars((V1Pod)pod, testEv));
-            A.CallTo(() => _autoFake.Resolve<IKubernetesClient>().GetV1PodAsync(A<string>._, A<string>._, A<CancellationToken>._)).Returns((V1Pod)null);
+            A.CallTo(() => _autoFake.Resolve<IKubernetesClient>().CreateV1PodAsync(A<string>._, A<V1Pod>._, A<CancellationToken>._)).Invokes((pod) => CheckPodEnvVars(pod, testEv)).Returns(testPod);
+            A.CallTo(() => _autoFake.Resolve<IKubernetesClient>().GetV1PodAsync(A<string>._, A<string>._, A<CancellationToken>._)).ReturnsNextFromSequence(new[] { (V1Pod)null, createFakePodObject() });
         }
 
         // Verify if env vars in the pod spec match expectedEnvVars, throw exception if not.
-        private void CheckPodEnvVars(V1Pod pod, EnvironmentVariables testEv)
-        {        
+        private void CheckPodEnvVars(IFakeObjectCall pod, EnvironmentVariables testEv)
+        {
+            V1Pod actualPod = (V1Pod) pod.Arguments[1];
+            var actualContainerList = (IList<V1Container>) actualPod.Spec.Containers;
+            var actualEnvList = actualContainerList.First(c => c.Env != null).Env;
             List<string> expectedEnvVars = new List<string> {EnvironmentVariables.Names.CollectTelemetry, EnvironmentVariables.Names.ConsoleVerbosity, EnvironmentVariables.Names.CorrelationId};
 
-            foreach(var container in pod.Spec.Containers) 
-            {
-                Assert.Equal(expectedEnvVars.Count, container.Env.Count);
+                Assert.Equal(expectedEnvVars.Count, actualEnvList.Count);
                 Dictionary<string, string> podEnvVars = new Dictionary<string, string>();
-                foreach(var envVar in container.Env)
+                foreach(var envVar in actualEnvList)
                 {
-                    Assert.True(expectedEnvVars.Contains(envVar.Name));
+                    Assert.Contains(envVar.Name, expectedEnvVars);
                     podEnvVars.Add(envVar.Name, envVar.Value);
                 }
 
                 Assert.Equal(testEv.CollectTelemetry.ToString(), podEnvVars.GetValueOrDefault(EnvironmentVariables.Names.CollectTelemetry));
                 Assert.Equal(LoggingVerbosity.Verbose.ToString(), podEnvVars.GetValueOrDefault(EnvironmentVariables.Names.ConsoleVerbosity));
-                Assert.Equal(testEv.CorrelationId.ToString(), podEnvVars.GetValueOrDefault(EnvironmentVariables.Names.CorrelationId));
-            }
+                Assert.Equal("", podEnvVars.GetValueOrDefault(EnvironmentVariables.Names.CorrelationId));
+            
+        }
+
+        public V1Pod createFakePodObject()
+        {
+            V1Pod pod = new V1Pod();
+            pod.Status = new V1PodStatus();
+            pod.Status.ContainerStatuses = new List<V1ContainerStatus>();
+            pod.Status.Phase = "running";
+            pod.Metadata = new V1ObjectMeta();
+            pod.Metadata.Name = "testName";
+            pod.Metadata.NamespaceProperty = "testname";
+            pod.Spec = new V1PodSpec();
+            pod.Spec.Containers = new List<V1Container>();
+            V1Container v1Container = new V1Container();
+            v1Container.Name = "testContainer";
+            pod.Spec.Containers.Add(v1Container);
+            return pod;
         }
     }
 }
