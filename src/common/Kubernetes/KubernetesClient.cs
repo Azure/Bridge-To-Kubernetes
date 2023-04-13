98cdfabeffe3ede0c403b4f45c26cfd2f55ee2b4
Hariharan Subramanian <105889062+hsubramanianaks@users.noreply.github.com>
2023-04-13 15:35:49 -0400

    Update KubernetesClient.cs
    remove async since methods inside is not async.

--- src/common/Kubernetes/KubernetesClient.cs
+++ src/common/Kubernetes/KubernetesClient.cs
@@ -1008,7 +1008,7 @@ namespace Microsoft.BridgeToKubernetes.Common.Kubernetes
                 result.Add(output.Substring(0, equalIndex), output.Substring(equalIndex + 1));
             };
 
-            var gotEnvSuccessfully = await WebUtilities.RetryUntilTimeWithWaitAsync(async (i) =>
+            var gotEnvSuccessfully = await WebUtilities.RetryUntilTimeWithWaitAsync((i) =>
             {
                 var exitCode = this.RunShortRunningCommand(
                     KubernetesCommandName.GetContainerEnvironment,
