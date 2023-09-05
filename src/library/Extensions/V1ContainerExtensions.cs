5cc351992bdcac4c141c5133e62087cf97952c49
Hariharan Subramanian <hsubramanian@microsoft.com>
2023-09-05 11:26:36 -0400

    add linkerd to supported sidecars

--- src/library/Extensions/V1ContainerExtensions.cs
+++ src/library/Extensions/V1ContainerExtensions.cs
@@ -14,7 +14,8 @@ namespace k8s.Models
             return StringComparer.OrdinalIgnoreCase.Equals(container.Name, "devspaces-proxy")
                     || StringComparer.OrdinalIgnoreCase.Equals(container.Name, "istio-proxy")
                     || StringComparer.OrdinalIgnoreCase.Equals(container.Name, "daprd")
-                    || StringComparer.OrdinalIgnoreCase.Equals(container.Name, "jaeger-agent");
+                    || StringComparer.OrdinalIgnoreCase.Equals(container.Name, "jaeger-agent")
+                    || StringComparer.OrdinalIgnoreCase.Equals(container.Name, "linkerd-proxy");
         }
     }
 }
